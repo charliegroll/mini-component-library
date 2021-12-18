f46a5e3d8960c2f639f63c4cf85854fcf4a8ce1d
Charlie Groll <charliegroll@gmail.com>
2021-12-18 13:40:19 -0500

    Select: pt 1

--- src/components/Select/Select.js
+++ src/components/Select/Select.js
@@ -9,10 +9,32 @@ const Select = ({ label, value, onChange, children }) => {
   const displayedValue = getDisplayedValue(value, children);
 
   return (
-    <select value={value} onChange={onChange}>
-      {children}
-    </select>
+    <WrapperContainer>
+      <Wrapper value={value} onChange={onChange}>
+        {children}
+        <Icon id="chevron-down" size={6} />
+      </Wrapper>
+    </WrapperContainer>
   );
 };
+
+const WrapperContainer = styled.div`
+  position: relative;
+  width: 10px;
+`;
+
+const Wrapper = styled.select`
+  appearance: none;
+  border: none;
+  background: ${COLORS.transparentGray15};
+  border-radius: 8px;
+  color: ${COLORS.gray700};
+  padding: 12px;
+  width: auto;
+
+  :hover {
+    color: ${COLORS.black};
+  }
+`;
 
 export default Select;
